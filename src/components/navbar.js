7478abfd936d4f393da44eb2e783452ea07a628d
Adriano Barbet <abarbet21@gmail.com>
2022-10-30 01:02:49 +0100

    Some Stuff

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -2,7 +2,7 @@ import React from "react";
 import {Link} from "react-router-dom";
 
 
-export default function NavBar() {
+export default function NavBar(props) {
 
   const [count, setCount] = React.useState(1)
 
@@ -27,12 +27,14 @@ export default function NavBar() {
     }
   }
 
+  
+  console.log(props.element)
 
   return (
       <nav className="navbar-container">
         <div className="navbar-left">
           <Link to="/"><img onMouseOver={mouseEnter} onMouseLeave={mouseLeave} src={`/assets/me${face}.png`} alt=""/></Link>
-          <Link to="/"><p>Adriano</p></Link>
+          <Link to="/"><p style={{color: (props.path == "wefox" ? "white" : "white")}}>Adriano</p></Link>
         </div>
         <Link to="/resume"><p>Resume</p></Link>
       </nav>
--- src/pages/wefox.js
+++ src/pages/wefox.js
@@ -4,7 +4,31 @@ export default function Wefox() {
   return (
 
     <div className="wefox-container">
-
+      <img className="page-header-img" src="/assets/wefoxheader.png" alt="wefox"/>
+      <div>
+        <h1>Product Management Intern</h1>
+        <h2>I was part of the Customer Apps team in charge of the Iphone, Android, and Web apps.</h2>
+        <h4>Project Overview</h4>
+        <p>Creating from scratch a brand including all of the social media and community aspect. Designing and coding the website. Making all the free courses & workshops for students. Commmunicating with companies & organisations for partnerships and collaborative events. </p>
+        <h4>Skills & Tech Used</h4>
+        <div>
+          <ul className="left-list">
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+            <li>4</li>
+          </ul>
+          <ul className="right-list">
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+            <li>4</li>
+          </ul>
+        </div>
+        <img src="" alt="middle-img"/>
+        <h4>Project Definition</h4>
+        <p>Creating from scratch a brand including all of the social media and community aspect. Designing and coding the website. Making all the free courses & workshops for students. Commmunicating with companies & organisations for partnerships and collaborative events. </p>
+      </div>
     </div>
 
 
